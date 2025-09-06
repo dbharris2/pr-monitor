c5b65a8300318b5d7efcd41c822b5dc57d21e8e1
Devon Mars <dbharris2@gmail.com>
2025-09-06 10:56:27 -0600

    feat: Next 15, React 19, Relay 20

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <link href="code-review.svg" rel="icon" type="image/svg+xml"></link>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              try {
+                const theme = localStorage.getItem('theme');
+                if (theme === 'dark') {
+                  document.documentElement.classList.add('dark');
+                }
+              } catch (e) { console.error(e); }
+            `
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <div className="h-full min-h-screen bg-gray-50 dark:bg-gray-900">
           <RelayContextProvider>{children}</RelayContextProvider>
