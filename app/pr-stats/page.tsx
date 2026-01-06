9c80527aabe039722d5abaa2085cd0aabc1a0fa1
Devon Mars <dbharris2@gmail.com>
2026-01-06 12:45:52 -0700

    chore: Remove pr-stats

--- app/pr-stats/page.tsx
+++
@@ -1,9 +0,0 @@
-import { cookies } from 'next/headers';
-
-import PrStatsPage from 'components/pr-stats-page';
-
-export default async function Page() {
-  const cookieStore = await cookies();
-  const hasToken = cookieStore.has('gh_token');
-  return <PrStatsPage initialHasToken={hasToken} />;
-}
