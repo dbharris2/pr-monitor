dc8b0a8b4957c0491c0a9448ccc47eef977c40d8
Devon <dbharris2@gmail.com>
2026-01-16 09:05:35 -0700

    feat: Minor UI changes (#27)

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,13 +1,19 @@
+import cn from 'utils/cn';
+
 type Props = {
   src: string;
   title?: string;
+  className?: string;
 };
 
-export const Avatar = ({ src, title }: Props) => (
+export const Avatar = ({ src, title, className }: Props) => (
   // eslint-disable-next-line @next/next/no-img-element
   <img
     alt={title}
-    className="size-5 rounded-xl border-2 border-solid border-stone-700 dark:border-catppuccin-subtext0"
+    className={cn(
+      'size-5 rounded-full border-2 border-solid border-stone-700 dark:border-catppuccin-subtext0',
+      className
+    )}
     src={src}
     title={title}
   />
